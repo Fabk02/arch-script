40e23c082b1cd8918797e6091bb3306e012dfd4c
Fabk02 <74472871+Fabk02@users.noreply.github.com>
2024-03-29 12:40:59 +0100

    Add files via upload

--- Arch_5_locale_config.sh
+++ Arch_5_locale_config.sh
@@ -3,6 +3,6 @@ sed -i 's/#en_US ISO-8859-1/en_US ISO-8859-1/' /etc/locale.gen
 sed -i 's/#it_IT.UTF-8 UTF-8/it_IT.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/#it_IT ISO-8859-1/it_IT ISO-8859-1/' /etc/locale.gen
 locale-gen
-echo "export LANG=\"en_US.UTF-8\"" >> /etc/locale.conf
-echo "export LC_COLLATE=\"C\"" >> /etc/locale.conf
-echo "export KEYMAP=\"it\"" >> /etc/vconsole.conf
+echo "LANG=\"en_US.UTF-8\"" >> /etc/locale.conf
+echo "LC_COLLATE=\"C\"" >> /etc/locale.conf
+echo "KEYMAP=\"it\"" >> /etc/vconsole.conf
